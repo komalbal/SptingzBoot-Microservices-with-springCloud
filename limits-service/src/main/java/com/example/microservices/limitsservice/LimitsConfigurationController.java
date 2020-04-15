0dc2d647cc64ce8ac6c7f599be7ac60bed5e4053
kbaldawa <kbaldawa@KBALDAWA-IN.in.oracle.com>
2020-04-15 14:49:56 +0530

    fault tollerence with hystrix

--- limits-service/src/main/java/com/example/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -15,6 +16,16 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration getLimits() {
 		return new LimitConfiguration(config.getMaximum(),config.getMinimum());
+	}
+	
+	@GetMapping("/fault-tollerence-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetriveConfiguration")
+	public LimitConfiguration retriveConfiguration() {
+		throw new RuntimeException("Not Avaailable");
+	}
+	
+	public LimitConfiguration fallbackRetriveConfiguration() {
+		return new LimitConfiguration(9999, 9);
 	}
 
 }
